96116402c24f410bfe4ed5c2aca5388f2055c0dc
Pod 054 <pod-054@outlook.com>
2020-05-17 17:58:32 +0800

    modified

--- function.ts
+++ function.ts
@@ -1,8 +1,31 @@
 // TypeScript functions can be created both as a named function or as an anonymous function.
-function buildName(firstName: string, lastName = "Smith") {
-    return firstName + " " + lastName;
+// this and arrow functions
+interface Card {
+    suit: string;
+    card: number;
 }
 
-let result = buildName("Bob", "Adams");
+interface Deck {
+    suits: string[];
+    cards: number[];
+    createCardPicker(this: Deck): () => Card;
+}
 
-console.log(result);
+let deck: Deck = {
+    suits: ["hearts", "spades", "clubs", "diamonds"],
+    cards: Array(52),
+    createCardPicker: function(this: Deck) {
+        return () => {
+            let pickedCard = Math.floor(Math.random() * 52);
+            let pickedSuit = Math.floor(pickedCard / 13);
+
+            return { suit: this.suits[pickedSuit], card: pickedCard % 13};
+        }
+    }
+}
+
+let cardPicker = deck.createCardPicker();
+let pickedCard = cardPicker();
+
+alert("card: " + pickedCard.card + " of " + pickedCard.suit);
+// console.log(typeof(pickedCard));
